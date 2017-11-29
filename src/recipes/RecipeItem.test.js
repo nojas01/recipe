ef027063c5c47311bade147bdba74a50c611478f
Wouter de Vos <wouter@codaisseur.com>
2017-11-29 12:09:20 +0100

    Upgrade test dependencies to work with React 16

--- src/recipes/RecipeItem.test.js
+++ src/recipes/RecipeItem.test.js
@@ -4,9 +4,7 @@ import chai, { expect } from 'chai'
 import { shallow } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import RecipeItem from './RecipeItem'
-import Vegan from '../images/vegan.svg'
-import Vegetarian from '../images/vegetarian.svg'
-import Pescatarian from '../images/pescatarian.svg'
+import Title from '../components/Title'
 
 chai.use(chaiEnzyme())
 
@@ -21,26 +19,12 @@ const recipe = {
 describe('<RecipeItem />', () => {
   const container = shallow(<RecipeItem { ...recipe } />)
 
-  it('is wrapped in a article tag with class name "recipe"', () => {
+  it('is wrapped in a article tag with class name "RecipeItem"', () => {
     expect(container).to.have.tagName('article')
     expect(container).to.have.className('RecipeItem')
   })
 
-  it('contains a the title', () => {
-    expect(container.find('h1')).to.have.text(recipe.title)
-  })
-
-  it("shows a Vegetarian icon  when it is vegetarian", () => {
-    expect(container.find('ul > li > img')).to.have.attr('src', Vegetarian)
-  })
-
-  it("shows a Vegan icon  when it is vegan", () => {
-    const container = shallow(<RecipeItem { ...recipe } vegan={true} />)
-    expect(container.find('ul > li > img')).to.have.attr('src', Vegan)
-  })
-
-  it("shows a Pescatarian icon  when it is pescatarian", () => {
-    const container = shallow(<RecipeItem { ...recipe } pescatarian={true} vegetarian={false} />)
-    expect(container.find('ul > li > img')).to.have.attr('src', Pescatarian)
+  it('contains a title', () => {
+    expect(container).to.contain(<Title content={recipe.title} className="level-2" />)
   })
 })
